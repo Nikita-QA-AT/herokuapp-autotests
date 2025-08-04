04588266e1b12697b3468cdd2af7b57a77b24a7c
Nikita-QA-AT <olkin.nikita777@gmail.com>
2025-08-04 21:46:13 +0300

    triple added + triple delete

--- tests/test_add_remove.py
+++ tests/test_add_remove.py
@@ -1,5 +1,43 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
 
 def test_add_button(driver):
+    # открыли страницу
     driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
-    button_add_element = driver.find_element(By.CSS_SELECTOR, "button").click()
+
+
+    # кликнули по кнопке Add element 1 раз
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button"))
+    ).click()
+
+    # кликнули по кнопке Add element 2 раз
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button"))
+    ).click()
+
+    # кликнули по кнопке Add element 3 раз
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button"))
+    ).click()
+    time.sleep(1)
+
+    # нашли все кнопки delete
+    delete_buttons = driver.find_elements(By.CSS_SELECTOR, ".added-manually")
+    assert len(delete_buttons) == 3
+
+
+    # проверили, что появилась кнопка delete
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#elements .added-manually"))
+    )
+
+    # Проверяем текст кнопки
+    button_delete = driver.find_element(By.CSS_SELECTOR, ".added-manually")
+    assert button_delete.text == "Delete"
+
+    for btn in delete_buttons:
+        btn.click()
+        time.sleep(1)
